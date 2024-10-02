438fdba4eedc9c33d2833d754bbf7246c562725b
amadeus211 <malyuk2004.mail.ru@gmail.com>
2024-10-02 16:29:40 +0300

    first

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Bank {
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Bank(double init_balance) {
+        if(init_balance<0){
+            throw new IllegalArgumentException("Initial balance cannot be negative");
+        }else{
+            balance = init_balance;
+        }
+    }
+
+    public void plus_balance(double amount){
+        if(amount<0){
+            throw new IllegalArgumentException("Amount must be positive");
+        }else{
+            balance+=amount;
+        }
+    }
+
+    public void minus_balance(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+        balance -= amount;
+    }
+}
